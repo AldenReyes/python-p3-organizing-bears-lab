558abf597db8fb8c10a0e818b0d89d9792a12f5b
AldenReyes <122179486+AldenReyes@users.noreply.github.com>
2023-08-06 21:54:15 -0700

    Completes lab: Practices creating SQL tables and SQL queries

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 16, "M", "Brown", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 22, "F", "Brown", "Lively", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "F", "Black", "Timid", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 32, "M", "Brown", "Aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 9, "F", "White", "Calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 28, "M", "Black", "Aloof", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 29, "F", "Black", "Confident", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, NULL, NULL, NULL, NULL);
